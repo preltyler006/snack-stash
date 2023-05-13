a61e7946a810760f8aeda658262c11f3e65edd16
Tyler Prellwitz <tylerprellwitz@gmail.com>
2023-05-13 13:56:59 -0700

    Button code added, removing it due to complication

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-import os
-os.system('sudo pigpiod')
 from servo_test import servo_up, servo_down
 from rfid_read import read
-from rfid_write import write
+import RPi.GPIO as GPIO
+# from rfid_write import write
 import time
 # from led import red
 # from led import green
+from button import pressed
+# NEED TO RUN "sudo pigpiod" in console before running main.py
 
 current = None 
 
+
 while True:
     text = read()
+    button = pressed()
     # Where True == Up, False == Down
-    if text == "blue" and (current == None or current == False):
+    if (text == "up") and (button == False) and (current == None or current == False):
         current = True
         servo_up()
         print("UP")
-    elif text == "white" and (current == True):
+
+    elif (button == True) and (current == True):
         current = False
         servo_down()
         print("DOWN")
+
     else:
         time.sleep(1)
